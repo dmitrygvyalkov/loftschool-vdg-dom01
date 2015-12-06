4889814de5985e5d8bc3fcacf0d08744c401e699
vdg <dmitry.vyalkov@vdgperonal.ru>
2015-12-06 16:21:52 +0200

    Оформлена страница обратной связи для IE8

--- app/js/my.js
+++ app/js/my.js
@@ -1,6 +1,10 @@
 $(document).ready(function() { 
 
-
+	if (!Modernizr.placeholder) {
+		var scriptPlaceholder = document.createElement('script');
+		scriptPlaceholder.src = "bower/jquery.placeholder/jquery.placeholder.min.js";
+		document.body.appendChild(scriptPlaceholder);
+	}
 
 
 });
